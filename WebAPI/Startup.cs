5653136ad171434cfcb08f627c290f802aac8f4f
ahmetcaliskans <angelmetodiev@gmail.com>
2022-12-14 17:40:17 +0300

    deneme

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -66,6 +66,7 @@ namespace WebAPI
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        //Deneme
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
